b509f3d42c27a00851aa29721513660246ce0d92
Bramwell-Simpson <32172769+Bramwell-Simpson@users.noreply.github.com>
2021-01-14 21:48:10 +0000

    add blog to navbar

--- scalevideo.js
+++ scalevideo.js
@@ -2,7 +2,7 @@
     "use strict";
   
     // List of Video Vendors embeds you want to support
-    var players = ['iframe[src*="youtube.com"]', 'iframe[src*="vimeo.com"]'];
+    var players = ['iframe[src*="youtube.com"]', 'iframe[src*="vimeo.com"]', 'iframe[src*="youtube-nocookie.com"]'];
   
     // Select videos
     var fitVids = document.querySelectorAll(players.join(","));
